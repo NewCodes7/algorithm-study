50180396a87968ae799f34181cb0fd6ed47055cb
NewCodes7 <hani103201@naver.com>
2024-01-03 16:13:58 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 176 ms, Memory: 15744 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,46 +1,28 @@
+import java.io.*;
 import java.util.*;
+
 public class Main{
-    public static int[][] setArr(Scanner sc, int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                arr[i][j] = sc.nextInt();
+	public static void main (String args[]) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+        int[][] resultArr = new int[n][m];
+        
+        for (int i = 0; i < n * 2; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                resultArr[i % n][j] += Integer.parseInt(st.nextToken());
+                if (i >= n) {
+                    sb.append(resultArr[i % n][j]).append(" ");
+                }
+            }
+            if (i >= n) {
+                sb.append("\n");
             }
         }
         
-        return arr;
-    }
-    
-    public static int[][] plusArr(int [][] firstArr, int[][] secondArr) {
-        int[][] resultArr = new int[firstArr.length][firstArr[0].length];
-        for (int i = 0; i < resultArr.length; i++) {
-            for (int j = 0; j < resultArr[0].length; j++) {
-                resultArr[i][j] = firstArr[i][j] + secondArr[i][j];
-            }
-        }
-        
-        return resultArr;
-    }
-    
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-        int[][] firstArr = new int[n][m];
-        int[][] secondArr = new int[n][m];
-
-        
-        setArr(sc, firstArr);
-        setArr(sc, secondArr);
-        int[][] resultArr = plusArr(firstArr, secondArr);
-        
-        for (int i = 0; i < resultArr.length; i++) {
-            for (int j = 0; j < resultArr[0].length; j++) {
-                System.out.print(resultArr[i][j] + " ");
-            }
-            System.out.println();
-        }
-        
-
-
+        System.out.print(sb);
 	}
 }
