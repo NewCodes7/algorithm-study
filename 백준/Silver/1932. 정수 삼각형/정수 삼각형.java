c6d25de196e48508787f3d6a90578cd4f7220e29
NewCodes7 <hani103201@naver.com>
2024-01-22 09:04:44 +0900

    [Silver I] Title: 정수 삼각형, Time: 292 ms, Memory: 25888 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,20 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int n;
-    private static int[][] graph;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-        graph = new int[n][];
+        int n = Integer.parseInt(br.readLine());
+        int[][] graph = new int[n][];
         graph[0] = new int[] {Integer.parseInt(br.readLine())};
 
         for (int i = 1; i < n; i++) {
@@ -22,18 +14,18 @@ public class Main {
             int cnt = st.countTokens();
             int[] pair = new int[cnt];
 
-            for (int j = 0; j < cnt; j++) { // 메서드 기억.. 실시간으로 개수 변하나?
+            for (int j = 0; j < cnt; j++) {
                 int c = Integer.parseInt(st.nextToken());
-                int a = 0;
-                int b = 0;
+                int a = 0, b = 0;
                 if (j != 0) {
                     a = graph[i-1][j-1] + c;
                 }
-                if (j != cnt - 1) { // 여기도 마찬가지
+                if (j != cnt - 1) { 
                     b = graph[i-1][j] + c;
                 }
                 pair[j] = Math.max(a, b);
             }
+
             graph[i] = pair;
         }
 
