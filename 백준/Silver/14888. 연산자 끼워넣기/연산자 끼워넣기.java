fe609277e7514a0e3933864d4b37f2e8caf252af
NewCodes7 <hani103201@naver.com>
2024-02-08 17:32:48 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 164 ms, Memory: 19052 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,67 +1,60 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    private static int n;
-    private static List<String> op = new ArrayList<>();
-    private static List<Integer> numbers = new ArrayList<>();
-    private static List<Integer> repository = new ArrayList<>();
-    private static int result;
+public class Main {
+    private static int[] numbers;
+    private static int[] op;
+    private static List<Integer> answer = new ArrayList<>();
 
-    public static void permute(int start, int end) {
-        if (start == end) {
-            calculate();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        op = new int[4];
+        for (int i = 0; i < 4; i++) {
+            op[i] = Integer.parseInt(st.nextToken());
+        }
+
+        back(numbers[0], 0);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(Collections.max(answer)).append("\n").append(Collections.min(answer));
+        System.out.println(sb.toString());
+    }
+
+    public static void back(int sum, int depth) {
+        if (depth == numbers.length - 1) {
+            answer.add(sum);
             return;
         }
 
-        for (int i = start; i < end; i++) {
-            Collections.swap(op, start, i);
-            permute(start + 1, end);
-            Collections.swap(op, start, i); // backtrack
+        if (op[0] > 0) {
+            op[0]--;
+            back(sum + numbers[depth + 1], depth + 1);
+            op[0]++;
+        }
+        if (op[1] > 0) {
+            op[1]--;
+            back(sum - numbers[depth + 1], depth + 1);
+            op[1]++;
+        }
+        if (op[2] > 0) {
+            op[2]--;
+            back(sum * numbers[depth + 1], depth + 1);
+            op[2]++;
+        }
+        if (op[3] > 0) {
+            op[3]--;
+            back(sum / numbers[depth + 1], depth + 1);
+            op[3]++;
         }
     }
-
-    public static void calculate() {
-        result = numbers.get(0);
-        for (int i = 0; i < n-1; i++) {
-            if (op.get(i) == "+") {
-                result += numbers.get(i+1);
-            }
-            if (op.get(i) == "-") {
-                result -= numbers.get(i+1);
-            }
-            if (op.get(i) == "*") {
-                result *= numbers.get(i+1);
-            }
-            if (op.get(i) == "/") {
-                result /= numbers.get(i+1);
-            }
-        }
-        repository.add(result);
-    }
-
-    public static void main (String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            numbers.add(Integer.parseInt(st.nextToken()));
-        }
-
-        String[] oper = {"+", "-", "*", "/"};
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
-            int count = Integer.parseInt(st.nextToken());
-            while (count != 0) {
-                op.add(oper[i]);
-                count--;
-            }
-        }
-
-        permute(0, n-1);
-
-        System.out.print(Collections.max(repository) + "\n" + Collections.min(repository));
-     }
 }
