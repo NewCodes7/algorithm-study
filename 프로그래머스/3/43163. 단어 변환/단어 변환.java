50661286b6e62d45fc50851381c67079bbcf0f7d
NewCodes7 <hani103201@naver.com>
2024-02-13 16:17:20 +0900

    [level 3] Title: 단어 변환, Time: 0.27 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,73 +1,56 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
-    private static List<Integer> answer = new ArrayList<>(); // 선언!!
-    
-    public int solution(String begin, String target, String[] words) {
-        List<String> list = new ArrayList<>();
-        list.add(begin);
-        dfs(begin, target, words, list, 0);
-        if (answer.isEmpty()) {
-            return 0;
+
+    static class Node {
+        String next;
+        int edge;
+
+        public Node(String next, int edge) {
+            this.next = next;
+            this.edge = edge;
         }
-        return Collections.min(answer);
     }
-    
-    public static void dfs(String begin, String target, String[] words, List<String> list, int idx) {
-        if (list.get(list.size() - 1).equals(target)) {
-            answer.add(idx);
-            return;
-        }
-        
-        for (int i = 0; i < words.length; i++) {
-            if (list.contains(words[i])) continue;
-            if (checkWord(list.get(list.size() - 1), words[i])) {
-                list.add(words[i]);
-                dfs(begin, target, words, list, idx+1);
-                list.remove(words[i]); // 스트링 객체
+
+    public int solution(String begin, String target, String[] words) {
+        int n = words.length, ans = 0;
+
+        // for (int i=0; i<n; i++)
+        //  if (words[i] != target && i == n-1) return 0;
+
+        Queue<Node> q = new LinkedList<>();
+
+
+        boolean[] visit = new boolean[n];
+        q.add(new Node(begin, 0));
+
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            if (cur.next.equals(target)) {
+                ans = cur.edge;
+                break;
+            }
+
+            for (int i=0; i<n; i++) {
+                if (!visit[i] && isNext(cur.next, words[i])) {
+                    visit[i] = true;
+                    q.add(new Node(words[i], cur.edge + 1));
+                }
             }
         }
+
+        return ans;
     }
-    
-//     public static int bfs(String begin, String target, String[] words) {
-//         Queue<String> q = new LinkedList<>();
-//         q.offer(begin);
-//         int idx = 0;
-//         boolean isFounded = false;
-        
-//         while(!q.isEmpty()) {
-//             String c = q.poll();
-            
-//             if (c.equals(target)) {
-//                 isFounded = true;
-//                 break;
-//             }
-            
-//             for (int i = 0; i < words.length; i++) {
-//                 if (visited[i]) continue;
-//                 if (checkWord(c, words[i])) {
-//                     System.out.println(words[i]);
-//                     q.offer(words[i]);
-//                     visited[i] = true;
-//                     idx++;
-//                 }
-//             }
-//         }
-        
-//         if (!isFounded) idx = 0;
-        
-//         return idx;
-//     }
-    
-    public static boolean checkWord(String a, String b) {
+
+    static boolean isNext(String cur, String n) {
         int cnt = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) cnt++;
+        for (int i=0; i<n.length(); i++) {
+            if (cur.charAt(i) != n.charAt(i)) {
+                if (++ cnt > 1) return false;
+            }
         }
-        
-        if (cnt == 1) {
-            return true;
-        }
-        return false;
-    }
+
+        return true;
+    }    
 }
