53419709c8e2d8694b082a8565daec2e64b46eb0
NewCodes7 <hani103201@naver.com>
2024-02-13 17:03:12 +0900

    [level 3] Title: 네트워크, Time: 0.54 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,43 @@
+import java.util.*;
+
 class Solution {
-    private static boolean[] visited; 
-    // 아 컴퓨터는 3개 잖아. 모든 인접 행렬을 방문할 필요가 없어. 양방향 그래프이기에. 
-    
-    public static void dfs(int n, int[][] computers, int x) {
-        visited[x] = true;
-        
-        for (int i = 0; i < n; i++) {
-            int computer = computers[x][i];
-            if (computer == 1 && !visited[i] && x != i) {
-                dfs(n, computers, i); // y를 할 필요가 없는 듯?
-            } 
-        }
-    }
+    private static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(n, computers, i);
-                answer++;
-            }
+            if (visited[i]) continue;
+            bfs(i, n, computers);
+            answer++;
         }
         
         return answer;
     }
+    
+    public static void bfs(int start, int n, int[][] computers) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        
+        while (!q.isEmpty()) {
+            int c = q.poll();
+            visited[c] = true;
+            for (int i = 0; i < n; i++) {
+                if (i == c || visited[i]) continue;
+                if (computers[c][i] == 1 || computers[i][c] == 1) {
+                    q.offer(i);
+                }
+            }
+        }
+        
+    }
+    
 }
+
+// bfs를 여러번 (1번부터 n-1번까지) visited검사
+    // 해당 idx 노드  큐 넣기 
+    // while 반복하기 
+        // 방문처리하기
+        // 각 노드에 따라 가로줄과 세로줄 탐색 후 큐에 넣기
+// answer++
