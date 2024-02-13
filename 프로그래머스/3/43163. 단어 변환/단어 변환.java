50a89686a9b40f1ad2e86dbf3d181ca5ae942170
NewCodes7 <hani103201@naver.com>
2024-02-13 16:28:50 +0900

    [level 3] Title: 단어 변환, Time: 0.09 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,52 +1,45 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-
-    private class Node {
-        String next;
-        int edge;
-
-        public Node(String next, int edge) {
-            this.next = next;
-            this.edge = edge;
-        }
-    }
-
     public int solution(String begin, String target, String[] words) {
-        int n = words.length, ans = 0;
+        int[] vst = new int[words.length];
+        Queue<Integer> queue = new LinkedList<>();
 
-        Queue<Node> q = new LinkedList<>();
-
-        boolean[] visit = new boolean[n];
-        q.add(new Node(begin, 0));
-
-        while(!q.isEmpty()) {
-            Node cur = q.poll();
-            if (cur.next.equals(target)) {
-                ans = cur.edge;
-                break;
+        for ( int i = 0; i < words.length; i++ ) {
+            if ( vst[i] == 0 && diff(begin, words[i]) ) {
+                queue.offer(i);
+                vst[i] = 1;
             }
+        }
 
-            for (int i=0; i<n; i++) {
-                if (!visit[i] && isNext(cur.next, words[i])) {
-                    visit[i] = true;
-                    q.add(new Node(words[i], cur.edge + 1));
+        while ( !queue.isEmpty() ) {
+            int curIdx = queue.poll();
+            String curStr = words[curIdx];
+
+            if ( curStr.equals(target) )
+                return vst[curIdx];
+
+            for ( int j = 0; j < words.length; j++ ) {
+                if ( vst[j] == 0 && diff( curStr, words[j] ) ) {
+                    queue.offer(j);
+                    vst[j] = vst[curIdx] + 1;
                 }
             }
         }
 
-        return ans;
+        return 0;
     }
 
-    static boolean isNext(String cur, String n) {
-        int cnt = 0;
-        for (int i=0; i<n.length(); i++) {
-            if (cur.charAt(i) != n.charAt(i)) {
-                if (++ cnt > 1) return false;
-            }
+    private boolean diff(String str1, String str2) {
+        int diff = 0;
+
+        for ( int i = 0; i < str1.length(); i++ ) {
+            if ( str1.charAt(i) != str2.charAt(i) )
+                diff++;
+            if ( diff > 1 )
+                return false;
         }
 
-        return true;
-    }    
+        return diff==1;
+    }
 }
