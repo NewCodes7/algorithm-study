a91d6814aaef78a2c4ca1863642aca00a01421a7
NewCodes7 <hani103201@naver.com>
2024-02-14 09:05:47 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 2.97 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -8,10 +8,10 @@ class Solution {
             d -= deliveries[i];
             p -= pickups[i];
             
-            while(d < 0 || p < 0){
+            while(d < 0 || p < 0){ // 남은 cap으로 버틸때까지 버팀.
                 d += cap;
                 p += cap;
-                answer += (i+1)*2;
+                answer += (i+1)*2; // 어디까지 가느냐가 중요하기에 이건 빨리 결정이 남.
             }
         }
         return answer;
