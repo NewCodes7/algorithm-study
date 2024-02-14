2e144ab8f3a47bf05bad963181dfff6bb921101c
NewCodes7 <hani103201@naver.com>
2024-02-14 16:09:55 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,45 +1,57 @@
-import java.util.*;
-
 class Solution {
-    private static boolean[] visited;
-    private static int answer = 0;
+    private static int answer = 0; 
+    private static boolean[] visited; 
     
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        back(dungeons.length, new ArrayList<>(), k, dungeons);
+        back(k, dungeons, 0);
         return answer;
     }
     
-    public static void back(int n, List<Integer> list, int k, int[][] dungeons) {
-        if (list.size() == n) {
-            int hp = k;
-            int cnt = 0;
-            for (int i : list) {
-                if (hp >= dungeons[i][0]) {
-                    hp -= dungeons[i][1];
-                    cnt++;
-                } else {
-                    break;
-                }
-            }
-            answer = Math.max(answer, cnt);
+    public static void back(int k, int[][] dungeons, int depth) {
+        if (!checkGoing(k, dungeons)) {
+            answer = Math.max(answer, depth);
             return;
         }
         
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                list.add(i);
+        for (int i = 0; i < dungeons.length; i++) {
+            if (k >= dungeons[i][0] && !visited[i]) { // 여기 잘못함. 비지티드 낫
                 visited[i] = true;
-                back(n, list, k, dungeons);
-                list.remove(list.size() - 1);
+                back(k-dungeons[i][1], dungeons, depth+1);
                 visited[i] = false;
             }
-
+            
+            // if (i == dungeons.length - 1 && k < dungeons[i][0]) {
+            //     answer = Math.max(answer, depth);
+            //     return;
+            // }
         }
         
     }
+    
+    public static boolean checkGoing(int k, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (k >= dungeons[i][0] && !visited[i]) return true;
+        }
+        
+        return false;
+    }
 }
 
-// 지금 당장 체력을 덜 깎는 것만이 정답이 아니다. 
-// 개수도 적으니 완전탐색을 가는 게 좋겠다. 순열?
-// 순열 어떻게 하더라? 백? 
+// 체력 짱짱할 때 높은 곳 가면 좋지만, 많이 깎이면 안 가는 게 더 낫다. (우열을 당장 가리기 어려움)
+// 던전 개수가 8개이니 완탐해도 될 듯. 
+// 백트랙킹 섞기 (체력 마이너스면 후퇴)
+
+// 백트랙킹
+    // 종료조건: 체력이 음수이거나, 다 갔을 때
+        // depth 저장 Math.max 체려
+    // 반복문 
+        // 들어갈 수 있는지 확인
+        // 체력깎고 백트랙킹 시도
+        // 복구: 체력
+
+// 방문 배열이 필요해! 순열처럼!
+
+// 왜 실패?
+// 정상적으로 돌기는 할 듯. 그러면 종료 조건이 문제인가?
+// 더 이상 방문할 게 없을 때 , k가 음수가 될 수 없음!!
