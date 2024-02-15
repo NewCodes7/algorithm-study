6afdbe31a87501a6a2d6326ecf4fab3ef297b759
NewCodes7 <hani103201@naver.com>
2024-02-15 16:46:11 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.30 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 0.20 ms
+메모리: 74.6 MB, 시간: 0.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 12:17:4
+2024년 2월 4일 16:46:8
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,36 +1,35 @@
 import java.util.*;
 
 class Solution {
+    private static int answer = 0;
+    
     public int solution(int n, int k) {
-        int answer = 0;
-        char[] number = Integer.toString(n, k).toCharArray(); // 메서드 숙지!! 우선 의심되니 long으로
+        String[] c = Integer.toString(n, k).split("");
         
         StringBuilder sb = new StringBuilder();
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] == '0' && sb.toString().length() != 0) { // char 숫자인거 놓침
-                list.add(sb.toString());
-                sb = new StringBuilder();
-            }
-            if (number[i] != '0') {
-                sb.append(number[i]);
+        for (int i = 0; i < c.length; i++) {
+            if (c[i].equals("0") ) { // sb 길이 0일 떄!!!!
+                if (sb.length() == 0) continue;
+                check(Long.parseLong(sb.toString())); // 왜 오류?
+                sb.setLength(0); // 초기화 방법
+            } else {
+                sb.append(c[i]);
             }
         }
-        if (sb.toString().length() != 0) list.add(sb.toString());
         
-        for(String c : list) {
-            if (isPrime(Long.parseLong(c))) answer++; // 여기서 런타임에러.. 음. long으로 했어야 함.
-        }
+        if (sb.length() != 0) check(Long.parseLong(sb.toString()));
         
         return answer;
     }
     
-    public static boolean isPrime(long n) {
-        if (n == 1) return false;
-        for (long i = 2; i <= Math.sqrt(n); i++) { // 이유?
-            if (n % i == 0) return false;
+    public static void check(long n) {
+        if (n == 1) return;
+        for (long i = 2; i <= Math.sqrt(n); i++) { // 등호
+            if (n % i == 0) return;
         }
         
-        return true;
+        answer++;
     }
 }
+
+// 런타임 에러 외우자.. long 자료형
