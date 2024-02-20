ea3ed6e460e77fc807475cf91c79d57175d6c492
NewCodes7 <hani103201@naver.com>
2024-02-20 09:35:33 +0900

    [level 2] Title: 타겟 넘버, Time: 0.29 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.58 ms
+메모리: 76 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 11:49:41
+2024년 2월 2일 9:35:30
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,37 @@
-import java.util.*;
-
 class Solution {
     private static int answer = 0;
     
-    public static void dfs(int[] numbers, int target, int depth, int sum) {
-        if (depth == numbers.length) {
-            if (target == sum) answer++;
-            return;
-        } else {
-            dfs(numbers, target, depth+1, sum + numbers[depth]);
-            dfs(numbers, target, depth+1, sum - numbers[depth]);
-        }
-    }
-    
     public int solution(int[] numbers, int target) {
+        int targetSum = 0;
+        for (int c : numbers) targetSum += c;
+        targetSum = (targetSum - target) / 2;
         
-        dfs(numbers, target, 0, 0);
+        comb(0, targetSum, numbers, 0);
         
         return answer;
     }
+    
+    public static void comb(int sum, int targetSum, int[] numbers, int idx) {
+        if (sum >= targetSum) {
+            if (sum == targetSum) answer++;
+            return;
+        }
+        
+        for (int i = idx; i < numbers.length; i++) {
+            comb(sum + numbers[i], targetSum, numbers, i+1); // idx+1이 아니라 i+1!!
+        }
+    }
 }
+
+
+/*
+9:16~
+
+총합 - 2 * (고른 합) == target
+
+고른 합 = (총합 - target)/2
+
+고른 합의 경우의 수가 답.
+
+조합 통해서 해당 합 경우의 수 찾기
+*/
