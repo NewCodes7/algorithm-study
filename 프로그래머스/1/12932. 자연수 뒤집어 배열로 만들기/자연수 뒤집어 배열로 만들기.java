dff68ee9fda6cd1b72957ef05587bd4bc30d74c8
H-server <hani103201@naver.com>
2024-02-20 17:09:20 +0900

    입국심사 피드백

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++
@@ -1,8 +0,0 @@
-import java.util.stream.Stream;
-
-class Solution {
-    public int[] solution(long n) {
-        String m = new StringBuffer(n+"").reverse().toString();
-        return  Stream.of(m.split("")).mapToInt(Integer::parseInt).toArray();
-    }
-}
--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++
@@ -1,7 +0,0 @@
-SELECT *
-FROM PLACES
-WHERE HOST_ID IN (SELECT HOST_ID
-                FROM PLACES
-                GROUP BY HOST_ID
-                HAVING COUNT(HOST_ID) >= 2)
-ORDER BY ID
