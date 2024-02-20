13b09a706960e2bd52f3a7c52a9fab26beb64361
NewCodes7 <hani103201@naver.com>
2024-02-20 10:49:42 +0900

    [level 3] Title: 아이템 줍기, Time: 0.37 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
 
 ### 성능 요약
 
-메모리: 67.3 MB, 시간: 0.31 ms
+메모리: 72.5 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 10:19:31
+2024년 2월 2일 10:49:41
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,65 +1,104 @@
 import java.util.*;
 
 class Solution {
-    private static final int[] dx = {1, 0, -1, 0};
-    private static final int[] dy = {0, 1, 0, -1};
+    private static int[] dx = {1, 0, -1, 0};
+    private static int[] dy = {0, 1, 0, -1};
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
-        int[][] map = new int[51*2][51*2];
-        characterX *= 2;
-        characterY *= 2;
-        itemX *= 2;
-        itemY *= 2;
-        
-        for (int[] r : rectangle) {
-            r[0] *= 2;
-            r[1] *= 2;
-            r[2] *= 2;
-            r[3] *= 2;
-            for (int i = r[0]; i <= r[2]; i++) { // 가로 처리
-                map[i][r[1]] += 1;
-                map[i][r[3]] += 1;
-            }
-            for (int j = r[1] + 1; j < r[3]; j++) { // 세로 처리
-                map[r[0]][j] += 1;
-                map[r[2]][j] += 1;
-            }
-            for (int k = r[0]+1; k < r[2]; k++) { // 내부 처리
-                for (int l = r[1]+1; l < r[3]; l++) {
-                    map[k][l] += 3;
-                }
-            }
-        }
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {characterX, characterY, 0}); // 비용까지! 배열말고도 클래스 별도 선언 가능.
-        map[characterX][characterY] = 0; // 방문처리
+        int[][] map = makeMap(rectangle);
 
-        while(!q.isEmpty()) {
-            int[] c = q.poll();
-            if (c[0] == itemX && c[1] == itemY) {
-                answer = c[2] / 2; 
-                break;
-            }
-            
-            for(int i = 0; i < 4; i++) {
-                int nx = c[0] + dx[i];
-                int ny = c[1] + dy[i];
-                
-                if (nx < 0 || nx > 100 || ny < 0 || ny > 100) continue;
-                
-                if (map[nx][ny] == 1 || map[nx][ny] == 2) {
-                    q.offer(new int[] {nx, ny, c[2] + 1});
-                    map[nx][ny] = 0;
-                }
-            }
-        }
+        answer = bfs(map, characterX*2, characterY*2, itemX*2, itemY*2);
         
-        // 0 : 기본 (지날 수 없는 길) & 방문처리
-        // 1 : 지날 수 있는 길
-        // 2 : 코너
-        // 3이상 : 직사각형 내부
         return answer;
     }
+    
+    public static int[][] makeMap(int[][] rectangle) {
+        int[][] map = new int[101][101]; // 102가 아니라 101 
+        
+        // 좌표 2배 처리
+        for (int[] c : rectangle) {
+            c[0] *= 2;
+            c[1] *= 2;
+            c[2] *= 2;
+            c[3] *= 2;
+        }
+        
+        // 선분 및 꼭짓점 1 처리
+        for (int[] c : rectangle) {
+            
+            // 가로
+            for (int i = c[0]; i <= c[2]; i++) {
+                map[i][c[1]] = 1;
+                map[i][c[3]] = 1;
+            }
+            
+            // 세로
+            for (int i = c[1]+1; i < c[3]; i++) {
+                map[c[0]][i] = 1;
+                map[c[2]][i] = 1;
+            }
+        }
+        
+        // 내부 0 처리
+        for (int[] c : rectangle) {
+            for (int i = c[0]+1; i < c[2]; i++) {
+                for (int j = c[1]+1; j < c[3]; j++) {
+                    map[i][j] = 0;
+                }
+            }
+        }
+        
+        return map;
+    }
+    
+    public static int bfs(int[][] map, int characterX, int characterY, int itemX, int itemY) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{characterX, characterY});
+        
+        while (!q.isEmpty()) {
+            int[] c = q.poll(); // 아 반복문 밖에 있어야지!! 매너리즘
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = c[0] + dx[i];
+                int ny = c[1] + dy[i];
+
+                if (nx < 2 || nx > 100 || ny < 2 || ny > 100) continue; // 101의 두배인 202라 해버림..  0이 아니라 1임.. 이거 조심하자. 실수하기 쉽겠다. 
+
+                if (map[nx][ny] == 1) {
+                    map[nx][ny] = map[c[0]][c[1]] + 1;
+                    q.offer(new int[]{nx, ny});
+                }
+            }
+        }
+
+        return (map[itemX][itemY]-1)/2;
+    }
 }
+
+/*
+9:41~ 10:08
+
+경로 표시하기
+int - 1 길 / 0 막힘
+직사각형 꼭짓점. 
+직사각형 내부는 0으로
+직사각형 선분 및 꼭짓점은 1로
+
+한 번이라도 0 할당되면 영원히 1
+
+1 처리 싹 진행한다음, 내부 0처리는 마지막에. 따로.
+
+지도 크기 51
+
+전처리
+    1 처리
+    0 처리
+
+bfs 최단 경로
+    최단 거리는 지도에 저장하면 됨.
+    1일 때만 진행
+    
+
+
+*/
