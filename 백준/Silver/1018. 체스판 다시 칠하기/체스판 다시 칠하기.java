7f073ac95bb1f43816dcbf17fc4a7fafbfb02500
NewCodes7 <hani103201@naver.com>
2024-02-22 11:27:40 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -2,74 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static int n, m;
+    private static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int[][] board = new int[n][m];
-
+        int[][] graph = new int[n][m];
         for (int i = 0; i < n; i++) {
-//            st = new StringTokenizer(br.readLine(), ""); // 구분자 이렇게 하면 안 되나?
-            String c = br.readLine();
+            String s = br.readLine();
             for (int j = 0; j < m; j++) {
-                if (c.charAt(j) == 'W') {
-                    board[i][j] = 0;
+                if(s.charAt(j) == 'W') {
+                    graph[i][j] = 1;
                 } else {
-                    board[i][j] = 1;
+                    graph[i][j] = 0;
                 }
             }
         }
 
-        int w = getCount(checkBoard(board, 0)); // 보드 계속 연속해서 사용할 수 없음 주소 같아서.
-        int b = getCount(checkBoard(board, 1));
-        System.out.println(Math.min(w, b));
+        // 8,1 9,2 10,3
+        for (int i = 0; i < n-7; i++) {
+            for (int j = 0; j < m-7; j++) {
+                count(graph, new int[]{i, j});
+            }
+        }
+
+        System.out.println(answer);
     }
 
-    public static int[][] checkBoard(int[][] board, int color) { // 구현은 문제를 잘 읽는 게 정말 중요...
-        int unit = color;
-        int[][] chess = new int[board.length][board[0].length]; // 새로 생성.
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (j % 2 == 0 && board[i][j] != unit) {
-                    chess[i][j] = 2;
-                }
-                if (j % 2 == 1 && board[i][j] == unit) {
-                    chess[i][j] = 2;
-                }
-            }
+    public static void count(int[][] graph, int[] start) {
+        int res = 0;
+        int res2 = 0;
 
-            unit = color;
-            if (i % 2 == 0) {
-                unit++;
-                if (unit == 2) {
-                    unit = 0;
+        for (int i = start[0]; i < start[0]+8; i++) {
+            for (int j = start[1]; j < start[1]+8; j++) {
+                if (i % 2 == 0 && j % 2 != graph[i][j]) { // 0, 1, 0 이 아니라면
+                    res++;
+                }
+                if (i % 2 == 1 && j % 2 == graph[i][j]) { // 0, 1, 0 이라면
+                    res++;
+                }
+
+                if (i % 2 == 0 && j % 2 == graph[i][j]) { // 0, 1, 0 이 아니라면
+                    res2++;
+                }
+                if (i % 2 == 1 && j % 2 != graph[i][j]) { // 0, 1, 0 이라면
+                    res2++;
                 }
             }
         }
-        return chess;
-    }
 
-    public static int getCount(int[][] chess) {
-        int min = (int) 1e9;
-        for (int i = 8; i <= chess.length; i++) {
-            for (int j = 8; j <= chess[0].length; j++) {
-                min = Math.min(min, getCount2(chess, i, j));
-            }
-        }
-        return min;
-    }
-
-    public static int getCount2(int[][] chess, int n, int m) {
-        int count = 0;
-        for (int i = n - 8; i < n; i++) { // i = 0이라 해버림..
-            for (int j = m - 8; j < m; j++) {
-                if (chess[i][j] == 2) count++;
-            }
-        }
-        return count;
+        answer = Math.min(res, answer);
+        answer = Math.min(res2, answer);
     }
 }
+
+/*
+
+
+ */
