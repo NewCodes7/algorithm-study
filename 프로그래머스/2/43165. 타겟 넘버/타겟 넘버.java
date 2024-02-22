4fa943d394e07946f118b3b9911e7fef24bd9a83
NewCodes7 <hani103201@naver.com>
2024-02-22 10:24:23 +0900

    [level 2] Title: 타겟 넘버, Time: 0.44 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,37 +1,18 @@
 class Solution {
     private static int answer = 0;
-    
     public int solution(int[] numbers, int target) {
-        int targetSum = 0;
-        for (int c : numbers) targetSum += c;
-        targetSum = (targetSum - target) / 2;
-        
-        comb(0, targetSum, numbers, 0);
+        dfs(numbers, target, 0, 0);
         
         return answer;
     }
     
-    public static void comb(int sum, int targetSum, int[] numbers, int idx) {
-        if (sum >= targetSum) {
-            if (sum == targetSum) answer++;
+    public static void dfs(int[] numbers, int target, int depth, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) answer++;
             return;
         }
         
-        for (int i = idx; i < numbers.length; i++) {
-            comb(sum + numbers[i], targetSum, numbers, i+1); // idx+1이 아니라 i+1!!
-        }
+        dfs(numbers, target, depth+1, sum + numbers[depth]);
+        dfs(numbers, target, depth+1, sum - numbers[depth]);
     }
 }
-
-
-/*
-9:16~
-
-총합 - 2 * (고른 합) == target
-
-고른 합 = (총합 - target)/2
-
-고른 합의 경우의 수가 답.
-
-조합 통해서 해당 합 경우의 수 찾기
-*/
