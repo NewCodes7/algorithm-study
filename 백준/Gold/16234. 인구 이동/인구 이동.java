bb16cf8fe2c9a2150ecaabc747ac47e9739d0827
NewCodes7 <hani103201@naver.com>
2024-02-27 09:25:11 +0900

    [Gold IV] Title: 인구 이동, Time: 592 ms, Memory: 295532 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,93 +1,101 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     private static int n, l, r;
-    private static int[][] map;
-    private static List<int[]> union = new ArrayList<>();
-    private static boolean[][] visited;
+    private static int[][] graph;
     private static int[] dx = {1, 0, -1, 0};
     private static int[] dy = {0, 1, 0, -1};
+    private static boolean[][] visited;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         l = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
 
-        map = new int[n][n];
-        visited = new boolean[n][n];
+        visited = new boolean[n][n]; // 이거 빼먹엏서 Null에러 이런 거 빼먹지 좀 마!
+        graph = new int[n][n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int cnt = -1;
-        boolean isGoing = true;
-        while (isGoing) {
-            isGoing = false;
+        int time = 0;
+
+        while (true) {
+            boolean isMoved = false;
 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     if (!visited[i][j]) {
-                        int unionCnt = move(new int[] {i, j});
-                        if (unionCnt != 1) {
-                            isGoing = true;
-                        }
-                        union.clear();
+                        boolean temp = bfs(new int[] {i, j});
+                        if (temp) isMoved = true;
                     }
                 }
             }
 
-            cnt++;
-            visited = new boolean[n][n];
+            if (isMoved) {
+                visited = new boolean[n][n];
+                time++;
+            } else {
+                break;
+            }
         }
 
-        //했던 곳을 다시? 아 한 번의 시행에서 여러 연합군이 있을 수 있음.
-        System.out.println(cnt);
+        System.out.println(time);
     }
 
-    public static int move(int[] start) {
+    public static boolean bfs(int[] start) {
         Queue<int[]> q = new LinkedList<>();
         q.offer(start);
-        visited[start[0]][start[1]] = true; // false일 때로 인자 넘겨야함.
+        visited[start[0]][start[1]] = true;
 
-        int total = map[start[0]][start[1]];
-        int cnt = 1;
-        union.add(new int[] {start[0], start[1]});
+        boolean isMoved = false;
+        Queue<int[]> moveList = new LinkedList<>();
+        moveList.offer(start);
+        int total = graph[start[0]][start[1]];
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int[] c = q.poll();
 
             for (int i = 0; i < 4; i++) {
                 int nx = c[0] + dx[i];
                 int ny = c[1] + dy[i];
-                if (nx < 0 || nx > n-1 || ny < 0 || ny > n-1) continue;
-                if (visited[nx][ny]) continue;
 
-                int differ = Math.abs(map[c[0]][c[1]] - map[nx][ny]);
-                if (differ >= l && differ <= r) {
-                    visited[nx][ny] = true;
-                    q.offer(new int[] {nx, ny});
-                    total += map[nx][ny];
-                    cnt++;
-                    union.add(new int[] {nx, ny});
+                if (nx < 0 || nx > n-1 || ny < 0 || ny > n-1) continue;
+
+                if (!visited[nx][ny]) {
+                    int diff = Math.abs(graph[c[0]][c[1]] - graph[nx][ny]);
+                    if (diff >= l && diff <= r) {
+                        int[] next = new int[]{nx, ny};
+
+                        total += graph[nx][ny];
+                        moveList.offer(next);
+                        q.offer(next);
+                        visited[nx][ny] = true;
+                        isMoved = true;
+                    }
                 }
             }
         }
 
-        // 평균내기
-        if (cnt != 1) { // 이거 빠뜨림 ㅠㅠ
-            for (int i = 0; i < union.size(); i++) {
-                int avg = total / cnt;
-                map[union.get(i)[0]][union.get(i)[1]] = avg;
-            }
-        }
+        move(total, moveList);
 
-        return cnt;
+        return isMoved;
+    }
+
+    public static void move(int total, Queue<int[]> q) {
+        int avg = total / q.size();
+
+        while (!q.isEmpty()) { // good
+            int[] c = q.poll();
+            graph[c[0]][c[1]] = avg;
+        }
     }
 }
