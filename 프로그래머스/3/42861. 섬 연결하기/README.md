e22a1523b191c8518974940e415a7beaf37a1baf
NewCodes7 <hani103201@naver.com>
2024-02-27 10:54:29 +0900

    [level 3] Title: 섬 연결하기, Time: 0.68 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 0.66 ms
+메모리: 74.7 MB, 시간: 0.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 26일 20:45:56
+2024년 02월 27일 10:54:26
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,45 +1,56 @@
 import java.util.*;
 
 class Solution {
-    private int[] parent;
-
     public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
+
         int answer = 0;
-        
-        parent = new int[n];
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; ++i) {
+            graph[i] = new ArrayList<>();
         }
-        
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
-        
-        //Kruskal Algorithm
-        for(int i = 0; i < costs.length; i++) {
-            if(find(costs[i][0]) != find(costs[i][1])) {
-                union(costs[i][0], costs[i][1]);
-                answer += costs[i][2];
+
+        int count = 0;
+        for (int i = 0; i < costs.length; ++i) {
+            int v1 = costs[i][0];
+            int v2 = costs[i][1];
+            int cost = costs[i][2];
+
+            // 두 정점 연결 확인
+            boolean isConnected = false;
+            boolean[] visited = new boolean[n];
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(v1);
+            visited[v1] = true;
+
+            while (!queue.isEmpty()) {
+                int src = queue.poll();
+
+                for (int next : graph[src]) {
+                    if (next == v2) {
+                        isConnected = true;
+                        break;
+                    }
+
+                    if (!visited[next]) {
+                        visited[next] = true;
+                        queue.add(next);
+                    }
+                }
+            }
+
+            // 연결안 되어 있지 않은 경우 간선 적용
+            if (!isConnected) {
+                graph[v1].add(v2);
+                graph[v2].add(v1);
+                answer += cost;
+            }
+
+            if (count == n) {
+                break;
             }
         }
-        
+
         return answer;
     }
-
-        
-    public int find(int a) {
-        if(parent[a] == a) {
-            return a;
-        } else {
-            return parent[a] = find(parent[a]);
-        }
-    }
-    
-    public void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        
-        if(a != b) {
-            parent[b] = a;
-        }
-    }
-    
 }
