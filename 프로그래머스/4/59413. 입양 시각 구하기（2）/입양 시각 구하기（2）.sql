958644a68a00ea11b81d2b7bcb40fb07f6a85bfe
NewCodes7 <hani103201@naver.com>
2024-02-28 12:22:02 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,7 +1,10 @@
 SET @HOUR = -1;
+
 SELECT (@HOUR := @HOUR+1) AS HOUR, (SELECT COUNT(*)
                                    FROM ANIMAL_OUTS
                                    WHERE HOUR(DATETIME) = @HOUR) AS COUNT
 FROM ANIMAL_OUTS
 WHERE @HOUR < 23
 
+# 왜 이렇게 하면 안 되지?
+# 아.. 그러면 이거 행의 데이터가 24개 미만이면 틀리겠네!!!
