4b8947517119fb4710bc3e8349898c394abe507f
NewCodes7 <hani103201@naver.com>
2024-03-01 11:26:56 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 77.9 MB, 시간: 0.03 ms
+메모리: 78.6 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 8:46:21
+2024년 03월 01일 11:26:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,30 +1,86 @@
-import java.util.*;
-
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int index;
-        int move = name.length() - 1;
         
-        char[] nameChar = name.toCharArray();
+        char[] arr = name.toCharArray();
         
-        for (int i = 0; i < nameChar.length; i++) {
-            answer += getDiff(nameChar[i]);
-            
-            index = i + 1;
-            while (index < name.length() && nameChar[index] == 'A') {
-                index++;
+        int maxA = 0;
+        int temp = 1;
+        int idx = -1;
+        int size = 'Z' - 'A';
+        for (int i = 0; i < arr.length; i++) {
+            int d = arr[i] - 'A';
+            if (d > size/2) {
+                d = size - d + 1;
             }
+            answer += d;
             
-            move = Math.min(move, i * 2 + name.length() - index);
-            move = Math.min(move, (name.length() - index) * 2 + i);
+            if (i == arr.length -1) continue;
+            if (arr[i] == 'A' && arr[i+1] == 'A') {
+                temp++;
+            } else {
+                if (temp == 1) continue;
+                if (maxA < temp) {
+                    maxA = temp;
+                    idx = i - temp + 1;
+                    temp = 1;
+                }
+            }
         }
         
-        return answer + move;
-    }
-    
-    public static int getDiff(char c) {
-        int diff = Math.min(c - 'A', ('Z' - 'A' + 1) - (c - 'A'));
-        return diff;
+        if (answer == 0) return 0;
+
+        int dis = arr.length-1;
+        
+        // 쭉 앞으로
+        int str = arr.length-1;
+        for (int i = arr.length-1; i >=0; i--) {
+            if (arr[i] == 'A') {
+                str--;
+            } else {
+                break;
+            }
+        }
+        dis = Math.min(dis, str);
+        
+        // 쭉 뒤로
+        str = arr.length-1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == 'A') {
+                str--;
+            } else {
+                break;
+            }
+        }
+        dis = Math.min(dis, str);
+        
+        if (maxA > 1) {
+            dis = Math.min(dis, 2 * (idx-1) + arr.length - (idx+(maxA)));
+            dis = Math.min(dis, 2 * (arr.length - (idx+(maxA))) + idx-1);
+        }
+
+        answer += dis;
+        return answer;
     }
 }
+
+/*
+가장 먼저 현재 숫자 건들기
+왼쪽으로 갈지 오른쪽으로 갈지 판단
+
+오른쪽이 A라면 오른쪽 갈필요가 없음.
+    우선 한바퀴 미만으로 도는 게 목표.
+    
+반면, JBCDAAAAANA라면 왼쪽으로 가야 함. 
+A를 기준으로 섣불리 판단하기는 힘듦.
+
+막 왔다갔다 하는 건 말이 안되고
+결국 왼쪽으로 갔다가 오른쪽 갈건지 vs 오른쪽 갔다가 왼쪽 갈건지 정해야 함.
+아니면 쭉 오른쪽 or 쭉 왼쪽
+
+4가지 경우를 판단하면 될 듯. 
+
+그리고 A만드는 건 정해져 있음. 
+
+A가 한 개라면 왼쪽, 오른쪽 중 하나로 계산됨.
+*/
