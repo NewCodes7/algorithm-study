dacfb8f8b61df928e721a4c24fef105cb98e01b0
NewCodes7 <newcodes7@gmail.com>
2024-04-04 09:28:54 +0900

    [Silver II] Title: 연속합, Time: 260 ms, Memory: 22556 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,22 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력 받기
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] dp = new int[n];
-        dp[0] = Integer.parseInt(st.nextToken());
-        int max = dp[0];
+        // 배열, 최댓값 초기화
+        int[] sequence = new int[n];
+        sequence[0] = Integer.parseInt(st.nextToken());
+        int max = sequence[0];
 
         for (int i = 1; i < n; i++) {
-            dp[i] = Integer.parseInt(st.nextToken());
-            if (dp[i - 1] >= 0) dp[i] += dp[i - 1];
-            max = Math.max(max, dp[i]);
+            sequence[i] = Integer.parseInt(st.nextToken());
+
+            // 이전 값이 0보다 크면 연속해서 더할 필요 있음.
+            if (sequence[i - 1] >= 0) {
+                sequence[i] += sequence[i - 1];
+            }
+
+            max = Math.max(max, sequence[i]);
         }
 
         System.out.println(max);
