8e1d47646e9c9cc9a1c5099997b7899500f26712
NewCodes7 <newcodes7@gmail.com>
2024-04-09 08:47:07 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 152 ms, Memory: 14828 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력 받기
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        int[] dp2 = new int[n];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+
+                int a = n - 1 - i;
+                int b = n - 1 - j;
+                if (arr[a] > arr[b]) {
+                    dp2[a] = Math.max(dp2[a], dp2[b] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i] + dp2[i] + 1);
+        }
+
+        System.out.println(max);
+    }
+}
+
+/*
+
+ */
