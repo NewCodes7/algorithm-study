97988775ba21d6af0a02ef246f1e043ea5cce2e4
NewCodes7 <newcodes7@gmail.com>
2024-05-28 20:38:53 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.29 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,90 +1,100 @@
 import java.util.*;
 
 class Solution {
-    private static final int[] dx = {1, 0, -1, 0};
-    private static final int[] dy = {0, 1, 0, -1};
-    private static int[] result = new int[5];
-    private static boolean[][] visited = new boolean[5][5];
+    private static int[] dx = {1, 0, -1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+    private static boolean[][] visited;
     
     public int[] solution(String[][] places) {
-        Arrays.fill(result, 1);
+        int[] answer = new int[5];
+        for (int i = 0; i < 5; i++) {
+            answer[i] = 1;
+        }
         
-        for (int idx = 0; idx < 5; idx++) {
+        int index = 0;
+        for (String[] place : places) {
+            char[][] room = convert(place);
+            // System.out.println(Arrays.deepToString(room));
+            
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
-                    if (places[idx][i].charAt(j) == 'P') {
-                        dfs(places[idx], idx, 0, i, j);
+                    if (room[i][j] == 'P') {
                         visited = new boolean[5][5];
+                        if (bfs(room, i, j) == 0) {
+                            answer[index] = 0;
+                        };
                     }
                 }
             }
-        }
 
-        return result;
+            index++;
+        }
+        
+        return answer;
     }
     
-    public static void dfs(String[] place, int idx, int depth, int x, int y) {
-        // visited[x][y] = true; // 위치? 아 depth 3일 때 오작동 오류
+    public static char[][] convert(String[] arr) {
+        char[][] room = new char[5][5];
         
-        if (depth > 2 || place[x].charAt(y) == 'X') {
-            // System.out.println(0);
-            return;
-        }
-        
-        visited[x][y] = true;
-        
-        if (place[x].charAt(y) == 'P' && depth != 0) {
-            // System.out.println(idx+1 + " " + x + " "+ y);
-            result[idx] = 0;
-            return;
-        }
-        
-        depth++;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (!(nx < 0 || nx > 4 || ny < 0 || ny > 4) && !visited[nx][ny]) {
-                dfs(place, idx, depth, nx, ny);
+        for (int i = 0; i < 5; i++) {
+            char[] temp = arr[i].toCharArray();
+            
+            for (int j = 0; j < 5; j++) {
+                room[i][j] = temp[j];
             }
         }
+        
+        return room;
+    }
+    
+    public static int bfs(char[][] room, int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {x, y, 0}); // 좌표, 거리
+        visited[x][y] = true;
+        
+        while (!q.isEmpty()) {
+            int[] c = q.poll();
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = c[0] + dx[i];
+                int ny = c[1] + dy[i];
+                
+                if (nx < 0 || nx > 4 || ny < 0 || ny > 4) {
+                    continue;
+                }
+                
+                if (visited[nx][ny]) {
+                    continue;
+                }
+                
+                if (room[nx][ny] == 'P') {
+                    return 0;
+                } else if (room[nx][ny] == 'X') {
+                    continue;
+                }
+                
+                if (c[2] < 1) { // 2가 아니라 1까지만!!!!
+                    visited[nx][ny] = true;
+                    q.offer(new int[] {nx, ny, c[2] + 1}); // ++로 하면 안 되려나?
+                }
+                
+            }
+        }
+        
+        return 1;
     }
 }
 
 /*
-가는 경로중에 파티션이 없고 맨해튼 2라면, 검거
-바로 상하좌우 붙어있으면, 검거
-  *
- ***
-*****
- ***
-  *
-각 플레이어마다 이렇게 범위 생김. (13개)
- *
-***
- *
-이 단위가 반복됨. 재귀.
+똑같은 과정을 5번 반복하면 됨.
 
-반복문 places
-    반복문 place - 변수: 현재 depth(거리)
-        종료조건: depth > 2, 현재위치 파티션이라면, 
-        P가 맞다면 ㄴㄴㄴㄴ 이건 그 전에 확인했음!!!!
-            depth 0 아닌 게 확인됐다면, place종료, result 0 추가(boolean)
-            (depth++;)
-            반복문
-                좌표확인
-                dfs(상)
-                dfs(하)
-                dfs(좌)
-                dfs(우)
-        boolean == true 1추가
-        
-리팩토링
-    0을 더 검출했거나
-    덜 검출했거나..
-    
-    다시 제자리로 돌아올 수 있음.
-    방문처리? ㄴㄴ 힘듦.
-    
-    charAt은 리턴만?
-    문자열 특정 위치 바꾸기?
+2차원 배열로 만든 후, 완탐하면 되겠는데?
+
+bfs로 가능할 듯. visited 
+
+2차원 배열 완탐
+    P라면 bfs 시작
+        X가 나오면 그만.
+        거리가 2 초과라면 그만.
+
 */
