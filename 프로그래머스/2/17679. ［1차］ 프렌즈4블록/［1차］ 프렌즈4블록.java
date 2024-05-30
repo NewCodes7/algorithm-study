d6d348ee1e3691606fb9fc2e7302a9d033cf66ca
NewCodes7 <newcodes7@gmail.com>
2024-05-30 20:17:39 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.50 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,84 +1,99 @@
 import java.util.*;
 
 class Solution {
+    private static int m;
+    private static int n;
+
+    
     public int solution(int m, int n, String[] board) {
-        int answer = 0;
+        this.m = m;
+        this.n = n;
         
         // board를 2차원 배열로 변환
-        char[][] boardArr = new char[m][n];
+        char[][] gameBoard = new char[m][n];
         for (int i = 0; i < m; i++) {
             char[] temp = board[i].toCharArray();
             
             for (int j = 0; j < n; j++) {
-                boardArr[i][j] = temp[j];
+                gameBoard[i][j] = temp[j];
             }
         }
         
-        // 게임 시작
         boolean isGameActive = true;
-        int removedBlocks = 0;
+        int totalRemovedBlocks = 0;
         while (isGameActive) {
-            int beforeRemovedBlocks = removedBlocks;
-            boolean[][] fourBlocks = new boolean[m][n];
-                
-            // 4블록인지 check
-            for (int i = 0; i < m - 1; i++) {
-                for (int j = 0; j < n - 1; j++) {
-                    if (isFourBlock(boardArr, i, j)) {
-                        fourBlocks[i][j] = true;
-                        fourBlocks[i + 1][j] = true;
-                        fourBlocks[i][j + 1] = true;
-                        fourBlocks[i + 1][j + 1] = true;
-                    }
-                }
-            }
+            // 같은 모양의 블록인지 check
+            boolean[][] blocksToDelete = checkToDeleteBlocks(gameBoard);
             
             // 블록 지우기 & 떨어뜨리기
-            for (int i = 0; i < n; i++) { // 열
-                Deque<Character> dq = new ArrayDeque<>();
-                
-                for (int j = 0; j < m; j++) { // 행
-                    if (!fourBlocks[j][i]) {
-                        dq.offer(boardArr[j][i]);
-                    }
-                }
-                
-                // 순차적으로 떨어뜨리기
-                int index = m - 1;
-                while (!dq.isEmpty()) {
-                    boardArr[index][i] = dq.pollLast();
-                    index--;
-                }
-                
-                // 위에 빈 블록 X로 표현
-                for (int k = index; k >= 0; k--) {
-                    boardArr[k][i] = 'X';
-                    removedBlocks++;
-                }
-            }
+            int beforeRemovedBlocks = totalRemovedBlocks;
+            totalRemovedBlocks += removeBlocks(blocksToDelete, gameBoard);
             
-            // System.out.println(Arrays.deepToString(boardArr));
-            // break; // 임시!!!
-            
-            isGameActive = (beforeRemovedBlocks != removedBlocks);
+            // 삭제된 블록이 없다면 게임 종료하기 위함
+            isGameActive = (beforeRemovedBlocks != totalRemovedBlocks);
         }
         
-        return removedBlocks;
+        return totalRemovedBlocks;
     }
     
-    public static boolean isFourBlock(char[][] boardArr, int x, int y) {
-        if (boardArr[x][y] == 'X') {
+    public static boolean[][] checkToDeleteBlocks(char[][] gameBoard) {
+        boolean[][] blocksToDelete = new boolean[m][n];
+        
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (isSameBlock(gameBoard, i, j)) {
+                    blocksToDelete[i][j] = true;
+                    blocksToDelete[i + 1][j] = true;
+                    blocksToDelete[i][j + 1] = true;
+                    blocksToDelete[i + 1][j + 1] = true;
+                }
+            }
+        }
+        
+        return blocksToDelete;
+    }
+    
+    public static boolean isSameBlock(char[][] gameBoard, int x, int y) {
+        if (gameBoard[x][y] == 'X') { // 빈 블록인지 확인
             return false;
         }
         
-        if ((boardArr[x][y] == boardArr[x + 1][y]) 
-            && (boardArr[x][y + 1] == boardArr[x + 1][y + 1])) {
-            if (boardArr[x][y] == boardArr[x + 1][y + 1]) {
-                return true;
-            }
+        if ((gameBoard[x][y] == gameBoard[x + 1][y]) 
+            && (gameBoard[x][y + 1] == gameBoard[x + 1][y + 1])
+            && gameBoard[x][y] == gameBoard[x + 1][y + 1]) {
+            return true;
         }
         
         return false;
+    }
+    
+    public static int removeBlocks(boolean[][] blocksToDelete, char[][] gameBoard) {
+        int removedBlocks = 0;
+        
+        for (int i = 0; i < n; i++) { // 열
+            Deque<Character> dq = new ArrayDeque<>(); // 한 열에서 삭제되지 않을 블록 저장
+
+            for (int j = 0; j < m; j++) { // 행
+                if (!blocksToDelete[j][i]) { 
+                    dq.offer(gameBoard[j][i]);
+                }
+            }
+
+            // 한 열에서 블록들 순차적으로 떨어뜨리기
+            int index = m - 1;
+            while (!dq.isEmpty()) {
+                gameBoard[index][i] = dq.pollLast();
+                index--;
+            }
+
+            // 한 열에서 위에 빈 블록 X로 표현
+            for (int k = index; k >= 0; k--) {
+                gameBoard[k][i] = 'X'; // 빈 블록을 X로 표현
+                removedBlocks++;
+            }
+        }
+        
+        return removedBlocks;
     }
 }
 
@@ -101,4 +116,13 @@ board 방향이 주어지진 않았는데, 상관 없음. 위이든 아래이든
 
 블록을 지웠다면 어떻게 떨어뜨리지?
 열이 같은 것 기준으로 순회하면서 떨어뜨리면 됨. 
+
+45분, 1번 제출, 다시 X
+
+피드백
+1. 초반에 설계 잘했다. 12분 정도.
+2. 중간에 설계 잘못 한 부분 (블록 떨어뜨리기) 침착하게 잘 수정했다!
+3. 주석으로 잘 구분했다!
+
+
 */
