c4e96faa2c31c9c4140a8113b15b3bc176221444
NewCodes7 <newcodes7@gmail.com>
2024-06-06 20:45:17 +0900

    [level 2] Title: 양궁대회, Time: 0.44 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,127 +1,103 @@
 import java.util.*;
 
 class Solution {
-    private static int[] answer = null;
-    private static int[] rion = new int[11];
-    private static int rionS = 0;
-    private static int infoS = 0;
+    private int maxScoreGap = 1;
+    private int[] maxLionInfo = {-1};
+    private int n;
+    private int[] info;
     
     public int[] solution(int n, int[] info) {
-        back(n, info, 9); // 큰 점수"차!!!"로 이기려면 처음부터 시작해야함. 아니야.. 그 순서를 고려하려면 뒤에서부터 해야 해.,  // 0점은 더 맞출 필요가 없음 10은 버려도돼
+        this.n = n; 
+        this.info = info;
         
-        if (answer == null) {
-            answer = new int[1];
-            answer[0] = -1;
+        int scoreGap = 0;
+        for (int i = 0; i < 11; i++) {
+            if (info[i] != 0) {
+                scoreGap -= 10 - i;
+            }
         }
         
-        return answer;
+        back(0, scoreGap, 10, new int[11]);
+        
+        return maxLionInfo;
     }
-        public static void back(int n, int[] info, int depth) {
-        if (depth == -1 || n <= 0) {
-            checkWinner(n, info);
+    
+    private void back(int usedArrows, int scoreGap, int k, int[] lion) { // 어피치, 라이언 점수 구분 필요
+        if (usedArrows > n) {
             return;
         }
         
-        if (info[depth] == 0) {
-            rion[depth]++;
-            back(n-1, info, depth-1);
-            rion[depth]--;
-        } else {
-            if (n >= info[depth] + 1) { // 남은 화살 체크
-                rion[depth] += info[depth] + 1;
-                back(n-rion[depth], info, depth-1);
-                rion[depth] -= info[depth] + 1;
-            } else {
-                back(n, info, depth-1);
+        if (scoreGap >= maxScoreGap) {
+            // System.out.println(scoreGap);
+            int[] temp = lion.clone(); // 배열 복사!
+            temp[10] += n - usedArrows; // 아 할당이 아니라 +=로 했어야 함!! 방심 실수 ㅠㅠ 코드 하나하나!! 근데 그래도 2개 실패...
+            // 이 부분이 가장 위험 확률이 높을 듯. 
+            // 나중에 또 나올 수도 있을 듯. 9(10),0,0,1(0) -> 0,4(5),5(3),1(1),0(0) 이런 케이스 대략?
+            // 복잡해질 때쯤 실전에서는 다른 문제로 넘어가야 할 듯. 환기하는 걸 CS 문제로 대체해도 될 듯. 
+            
+            // 더 큰 점수차로 이길 수도 있는데 몰빵하는 게 맞아? -> ㅇㅇ. 더 큰 점수차는 백트랙킹에 의해 나오게 되니, 여유분이 있다면 몰빵하는 게 맞음.
+            if (scoreGap != maxScoreGap || maxLionInfo.length == 1) {
+                maxScoreGap = scoreGap;
+                maxLionInfo = temp;
             }
-        }
-        back(n, info, depth-1); // 반복문이 없어서.. back제대로 작동하지 않음. 풀없이 죽업버림.
-        
-    }
-    
-//     public static void back(int n, int[] info, int depth) {
-//         if (depth == n) { // depth가 0일 때 뿐만 아니라 끝에 도달했을 때도 계산해야함.
-//             checkWinner(n, info);
-//             return;
-//         }
-        
-//         for (int i = rion.length - 1; i >= 0; i--) { 
-//             // 우선 점수를 가장 많이 따는 데 집중!
-//             if (info[i] == 0) {
-//                 rion[i]++;
-//                 back(n, info, depth+1);
-//                 rion[i]--;
-//             } else {
-//                 if (n - depth >= info[i] + 1) { // 남은 화살 체크
-//                     rion[i] += info[i] + 1;
-//                     back(n, info, depth+info[i]+1);
-//                     rion[i] -= info[i] + 1;
-//                 }
-//             }
-//         }
-//     }
-    
-    public static void checkWinner(int n, int[] info) {
-        int rionScore = 0;
-        int infoScore = 0;
-        
-        // 점수 계산
-        for (int i = 0; i < 11; i++) {
-            if (rion[i] == 0 && info[i] == 0) continue;
-            if (rion[i] <= info[i]) {
-                infoScore += 10-i;
-            } else {
-                rionScore += 10-i;
+            
+            for (int i = 10; i >= 0; i--) {
+                if (temp[i] > maxLionInfo[i]) {
+                    maxLionInfo = temp;
+                    break;
+                } else if (temp[i] < maxLionInfo[i]) {
+                    break;
+                }
             }
         }
         
-        // 갱신
-        if (rionScore > infoScore && rionS-infoS < rionScore-infoScore) {
-            if (answer == null) {
-                answer = new int[11];
-                // answer = rion; 깊은 복사
+        for (int i = k; i >= 0; i--) {
+            usedArrows += info[10 - i] + 1;
+            lion[10 - i] += info[10 - i] + 1;
+            // scoreGap += k * 2; 이대로 하면 안 됨..! 어피치가 0개일 때는 k 하나만 더해야 해!
+            if (info[10 - i] == 0) {
+                scoreGap += i; // 아 여기도 k가 아니라 i!!!
+            } else {
+                scoreGap += i * 2;
             }
             
-            if (n > 0) { // 남은 화살이 있다면
-                rion[10] += n;
-            }
+            back(usedArrows, scoreGap, i - 1, lion); // k - 1이 아니라 i - 1!!!
             
-            for (int i = 0; i < 11; i++) {
-                answer[i] = rion[i];
+            usedArrows -= info[10 - i] + 1;
+            lion[10 - i] -= info[10 - i] + 1;
+            if (info[10 - i] == 0) {
+                scoreGap -= i;
+            } else {
+                scoreGap -= i * 2;
             }
-            rionS = rionScore;
-            infoS = infoScore;
         }
     }
 }
 
 /*
-n = 10
-길이 11
-11의 10제곱 / 10! 1,000,000,000 간당함. 우선 go
+<요구사항 정리>
+1. 특정 점수에 더 많은 화살을 맞춘 사람이 그 특정 점수를 가져감.
+    단, 특정 점수에 대해 화살 개수가 같으면 어피치가 가져감.
+2. 최종 점수로 승패 판결, 점수 같다면 어피치 승
+3. 가장 큰 점수차로 이길 수 있는 것 리턴
+    라이언 우승 방법 여러 개라면, 가장 낮은 점수를 더 많이 맞힌 경우 리턴
+        가장 낮은 점수에서 개수 같다면 그 다음걸로.
+4. n 최대 10개 -> 10H10 -> 19C10
 
-그 이모티콘 문제처럼.
+<설계> 21분
+10,000,000,000 대충 완탐해도 시간 제한은 안 걸릴 듯. 
 
-1. 백트랙킹
-    1. depth: 화살 개수
-    2. 종료조건: depth == n
-    3. 반복문: ++, back, 복구
-2. 계산
-    1. 이겼는지 판단 (반복문)
-    2. 기존의 것과 비교 후 저장
-3. 리턴 -> 값 없다면 -1반환
-
-거꾸로 가면, 실행횟수 줄일 수 있음. 가장 큰 점수차인걸 놓쳤다!!
-
-실패와 효율성 문제
-    1. 우선 back은 온전히 모든 경우의 수를 탐색함.
-    2. back의 성능을 높여야 할 듯. (굿판단)
-        1. 0일 때 하나만 추가해도 됨.
-        2. 점수가 부족하면서 1일 때 2만 추가해도 됨. 
-        3. 점수가 넉넉하면서 1일 때 0.
-        4. 그리고 10점에서 남는 화살을 제일 마지막에 부여하면 됨. -> calculate
-    3. 좀 더 성능 개선이 필요함.
+백트랙킹? 
+만약 어피치가 1개라도 쏜 영역에서 라이언이 쏜다면 '해당 개수 + 1'만큼 쏴야 함.
+조합? 
+    0개 이거나 (어피치 + 1)이여야 함.
+    단, 가장 큰 점수차이면서 화살 남았다면 제일 낮은 것에 할당
+그러면 가장 큰 점수차인 배열 어떻게 얻을거야? 
+    점수차 max인 것들로만 배열 따로 저장해서, 마지막에 상호간에 비교? 
     
-이제 실패 문제를 들여다보자.
-    왜 실패했을까?
+    가장 마지막에 나오는 게 정답 아닐까? 우선 구현 ㄱㄱ
+    
+55분 제출 2개 실패
+
+2개 실패 테케 생각해보자!!
 */
