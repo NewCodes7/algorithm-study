3a904ae0e624bc08705bf6a14674f50b36a92adc
Sanghyeok Kim <newcodes7@gmail.com>
2025-01-16 23:07:29 +0900

    [Silver IV] Title: 스택 2, Time: 4104 ms, Memory: 310200 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력 받기
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> q = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            if (input.length() == 1) { // length()
+                int result = 0;
+                if (input.equals("2")) {
+                    result = process2(q);
+                } else if (input.equals("3")) {
+                    result = process3(q);
+                } else if (input.equals("4")) {
+                    result = process4(q);
+                } else if (input.equals("5")) {
+                    result = process5(q);
+                }
+                System.out.println(result);
+            } else {
+                String[] arr = input.split(" "); // 더 나은 방법? 
+                process1(q, Integer.parseInt(arr[1]));
+            }
+        }
+    }
+
+    private static void process1(List<Integer> q, int X) {
+        q.add(X); // 이렇게 해도 되겠지 메모리 참조하니? 
+    }
+
+    private static int process2(List<Integer> q) {
+        int size = q.size();
+        if (size == 0) {
+            return -1;
+        } 
+
+        int result = q.get(size - 1);
+        q.remove(size - 1); // remove
+
+        return result;
+    }
+
+    private static int process3(List<Integer> q) {
+        return q.size();
+    }
+
+    private static int process4(List<Integer> q) {
+        return q.isEmpty() ? 1 : 0;
+    }
+
+    private static int process5(List<Integer> q) {
+        if (q.isEmpty()) {
+            return -1;
+        } 
+        return q.get(q.size() - 1);
+    }
+}
+
+/*
+10:50 ~ 
+
+1. parser
+2. stack 
+3. output
+
+개선하기 그 테이블 둬서! 
+ */
