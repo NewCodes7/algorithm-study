129b3a226ed000196c61c0ca21791822ea8d5dec
Sanghyeok Kim <newcodes7@gmail.com>
2025-01-27 11:49:05 +0900

    Update N과 M （1）.java

--- 백준/Silver III/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver III/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N, M;
-    static char[] arr;
-    static boolean[] ck;
-    static StringBuilder sb = new StringBuilder();
+    private static StringBuilder result = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        StringTokenizer input = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(input.nextToken());
+        int m = Integer.parseInt(input.nextToken());
 
-        arr = new char[2 * M];
-        ck = new boolean[N + 1];
-        back(0);
-        System.out.print(sb);
+        back(n, m, new StringBuilder(), 0);
+
+        System.out.println(result);
     }
 
-    static void back(int idx) {
-        if (idx == M) {
-            arr[2 * M - 1] = '\n';
-            sb.append(arr); // arr를 통째로
-            return ;
+    private static void back(int n, int m, StringBuilder sb, int depth) {
+        if (depth == m) {
+            result.append(sb.toString()).append("\n");
+            return;
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (!ck[i]) {
-                ck[i] = true; // 방문 처리
-                arr[2 * idx] = (char)(i + '0'); // append 편하게 하려고 ' ' 이것도 쓰려고 char로 쓴 듯.
-                arr[2 * idx + 1] = ' '; // 미리 출력 형식 고려
-                back(idx + 1);
-                ck[i] = false;
+        for (int i = 1; i <= n; i++) {
+            if (sb.toString().contains(Integer.toString(i))) {
+                continue;
             }
+
+            sb.append(i).append(" ");
+            back(n, m, sb, depth + 1);
+            sb.deleteCharAt(sb.length() - 1);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
