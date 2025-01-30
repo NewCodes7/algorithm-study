903d96fee3b734357dfb8a839f71ff8f64726857
Sanghyeok Kim <newcodes7@gmail.com>
2025-01-30 09:08:37 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     private static StringBuilder sb = new StringBuilder();
     private static int[] arr;
-    private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,14 +14,13 @@ public class Main {
         int n = Integer.parseInt(input.nextToken());
         int m = Integer.parseInt(input.nextToken());
         arr = new int[m];
-        visited = new boolean[n + 1];
 
-        dfs(n, m, 0);
+        dfs(n, m, 1,0);
 
         System.out.println(sb);
     }
 
-    private static void dfs(int n, int m, int depth) {
+    private static void dfs(int n, int m, int at, int depth) {
         if (depth == m) {
             for (int num : arr) {
                 sb.append(num).append(" ");
@@ -31,19 +29,9 @@ public class Main {
             return;
         }
 
-        int start;
-        if (depth == 0) {
-            start = 1;
-        } else {
-            start = arr[depth - 1];
-        }
-
-        for (int i = start; i <= n; i++) {
-            if (visited[i]) continue;
+        for (int i = at; i <= n; i++) {
             arr[depth] = i;
-            visited[i] = true;
-            dfs(n, m, depth + 1);
-            visited[i] = false;
+            dfs(n, m, i + 1, depth + 1);
         }
     }
 }
