024eba9d2cd81e114856bc784fbee5c7a4a05fb0
Sanghyeok Kim <newcodes7@gmail.com>
2025-02-14 09:03:21 +0900

    [Silver V] Title: 숫자 카드, Time: 888 ms, Memory: 135644 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                sb.append("1 ");
+            } else {
+                sb.append("0 ");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
