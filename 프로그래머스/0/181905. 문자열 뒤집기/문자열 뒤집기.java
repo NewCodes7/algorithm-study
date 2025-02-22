68166d78c7bc6038135efff4642d02dcbde9395b
Sanghyeok Kim <newcodes7@gmail.com>
2025-02-22 20:53:03 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.09 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        char[] arr = my_string.toCharArray();
+        char[] answer = new char[arr.length];
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (i >= s && i <= e) {
+                answer[e - (i - s)] = arr[i];
+            } else {
+                answer[i] = arr[i];
+            }
+        }
+        
+        return new String(answer);
+    }
+}
